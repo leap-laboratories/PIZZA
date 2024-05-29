6babf458b9ea81fb8aa288192630161ec797d3f2
atagade <atagade19@gmail.com>
2024-05-29 10:23:34 +0000

    Nearest changed to use similar logic as distant

--- attribution/token_perturbation.py
+++ attribution/token_perturbation.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
@@ -12,6 +12,7 @@ def get_replacement_token(
     perturbation_strategy: str,
     embeddings: np.ndarray = None,
     tokenizer: PreTrainedTokenizer = None,
+    n: int = -1,
 ) -> int:
     if perturbation_strategy == "fixed":
         return tokenizer.encode(FIXED_REPLACEMENT_TOKEN, add_special_tokens=False)[0]
@@ -20,9 +21,11 @@ def get_replacement_token(
             token_id_to_replace, embeddings, n_tokens=4
         )[-1]
     elif perturbation_strategy == "nearest":
-        return get_most_similar_token_ids(
-            token_id_to_replace, embeddings, tokenizer, 2
-        )[1]
+        sorted_tokens = sort_tokens_by_similarity(token_id_to_replace, embeddings)
+        pos = n
+        if pos < 0:
+            pos = len(sorted_tokens) + pos
+        return sorted_tokens[pos]
     else:
         raise ValueError(f"Unknown perturbation strategy: {perturbation_strategy}")
 
@@ -30,18 +33,20 @@ def get_replacement_token(
 def sort_tokens_by_similarity(
     reference_token_id: int,
     embeddings: np.ndarray,
-    n_tokens: int = 1,
-) -> List[int]:
+    n_tokens: Optional[int] = None,
+) -> np.ndarray:
     token_embedding = embeddings[reference_token_id, :]
 
     # Normalize the embeddings
-    normalized_embeddings = embeddings / np.linalg.norm(
-        embeddings, axis=1, keepdims=True
-    )
+    normalized_embeddings = embeddings / np.linalg.norm(embeddings, axis=1, keepdims=True)
     normalized_token_embedding = token_embedding / np.linalg.norm(token_embedding)
 
     # Compute the cosine similarity
     cosine_similarities = np.dot(normalized_embeddings, normalized_token_embedding)
+
+    # If n_tokens is None, return all tokens sorted by similarity
+    if n_tokens is None:
+        n_tokens = len(cosine_similarities)
 
     # Get the indices of the top n_tokens most similar tokens
     top_indices = np.argpartition(cosine_similarities, -n_tokens)[-n_tokens:]
